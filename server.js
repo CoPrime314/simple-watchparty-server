e520f93177bdae4899bd2756769d0597132f4d48
NathanThai2201 <thainhatanh2201@gmail.com>
2026-04-24 10:46:25 +0700

    up

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// In-memory room state
+const rooms = {};
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+
+    // Send current state to new user
+    if (rooms[roomId]) {
+      socket.emit("sync_state", rooms[roomId]);
+    }
+  });
+
+  socket.on("video_event", ({ roomId, action, time }) => {
+    // Save state
+    rooms[roomId] = {
+      action,
+      time,
+      updatedAt: Date.now(),
+    };
+
+    // Broadcast to everyone else
+    socket.to(roomId).emit("video_event", {
+      action,
+      time,
+      updatedAt: rooms[roomId].updatedAt,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
